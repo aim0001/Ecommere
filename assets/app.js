ed62e00ca0d81182855947ad1a26cc0efa25f34b
Aimane Bello <aimanebello100@gmail.com>
2024-04-30 14:33:49 +0200

    login and registration css design

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,41 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+    function showContent(tabId) {
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
+    tabContents.forEach(content => content.classList.remove('active-content'));
+
+    const selectedTab = document.getElementById(tabId);
+    selectedTab.classList.add('active-content');
+
+    const correspondingTab = Array.from(tabs).find(tab => tab.getAttribute('onclick').includes(tabId));
+    if (correspondingTab) {
+        correspondingTab.classList.add('active-tab');
+    }
+}
+
+//image defilement
+
+// script.js
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change d'image toutes les 2 secondes (2000 ms)
+}
+
+// Démarrez le diaporama lorsque la page est chargée
+window.onload = function () {
+    showSlides();
+};
