35b75003082b2036da7df99a044e81074b873634
Aimane Bello <aimanebello100@gmail.com>
2024-04-30 03:47:03 +0200

    Menu

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MenuController extends AbstractController
+{
+    #[Route('/', name: 'menu_Accueil')]
+    public function index(): Response
+    {
+
+        $menuItems=[
+            ['label'=>'Accueil', 'route'=>'menu_Accueil', 'class'=> 'menu_Accueil active'],
+            ['label'=>'Galerie_de_Meubles', 'route'=>'menu_Galerie', 'class'=> 'menu_Galerie'],
+            ['label'=>'Boutique', 'route'=>'menu_Boutique', 'class'=> 'menu_Boutique']
+        ];
+
+
+        return $this->render('menu/index.html.twig', [
+            'menuItems' => $menuItems,
+        ]);
+    }
+
+
+    #[Route('/galerie', name: 'menu_Galerie')]
+    public function gallerie(): Response
+    {
+
+        $menuItems=[
+            ['label'=>'Accueil', 'route'=>'menu_Accueil', 'class'=> 'menu_Accueil'],
+            ['label'=>'Galerie_de_Meubles', 'route'=>'menu_Galerie', 'class'=> 'menu_Galerie active'],
+            ['label'=>'Boutique', 'route'=>'menu_Boutique', 'class'=> 'menu_Boutique']
+        ];
+
+
+        return $this->render('menu/galerie.html.twig', [
+            'menuItems' => $menuItems,
+        ]);
+    }
+
+
+    #[Route('/boutique', name: 'menu_Boutique')]
+    public function boutique(): Response
+    {
+
+        $menuItems=[
+            ['label'=>'Accueil', 'route'=>'menu_Accueil', 'class'=> 'menu_Accueil'],
+            ['label'=>'Galerie_de_Meubles', 'route'=>'menu_Galerie', 'class'=> 'menu_Galerie'],
+            ['label'=>'Boutique', 'route'=>'menu_Boutique', 'class'=> 'menu_Boutique active']
+        ];
+
+
+        return $this->render('menu/boutique.html.twig', [
+            'menuItems' => $menuItems,
+        ]);
+    }
+}
